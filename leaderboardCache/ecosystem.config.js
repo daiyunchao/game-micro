29be4f66da13506cc201802d2a6ecfaf633a60a6
daiyunchao <yunchao.dai@me2zen.com>
2023-11-03 17:14:56 +0800

    fix

--- leaderboardCache/ecosystem.config.js
+++ leaderboardCache/ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
         max_memory_restart: "1G",
         env: {
           NODE_ENV: "production",
-          NODE_ID: "leaderboard_001"
+          NODE_ID: "leaderboardCache_001"
         }
       }
     ]
